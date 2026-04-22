9daf3d69aa942875c1583d45a3e7bb5731c9a2f8
Thanushan <thanushan.20240138@iit.ac.lk>
2026-04-22 18:37:00 +0530

    RoomsNotEmpty exceptionmapper was created

---
+++ src/main/java/com/smartcampus/mappers/RoomNotEmptyExceptionMapper.java
@@ -0,0 +1,19 @@
+package com.smartcampus.mappers;
+
+import com.smartcampus.exception.RoomNotEmptyException;
+import com.smartcampus.model.ErrorResponse;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+
+@Provider
+public class RoomNotEmptyExceptionMapper implements ExceptionMapper<RoomNotEmptyException> {
+
+    @Override
+    public Response toResponse(RoomNotEmptyException exception) {
+        ErrorResponse error = new ErrorResponse(409, "Conflict", exception.getMessage());
+        return Response.status(Response.Status.CONFLICT).entity(error).type(MediaType.APPLICATION_JSON).build();
+    }
+}
